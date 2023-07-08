10dbaff676522840688411079cb99e74e076fabe
M2iT073 <84164317+M2iT073@users.noreply.github.com>
2023-07-08 17:37:54 +0800

    Avatar Sprite

--- Assets/Scripts/JellyFish.cs
+++ Assets/Scripts/JellyFish.cs
@@ -31,6 +31,6 @@ public class JellyFish : BaseActor
     public void LightSphereControl()
     {
         // lightSphere.localScale = curSize * Vector3.one;
-        lightSphere.GetComponent<UnityEngine.Rendering.Universal.Light2D>().RadiusOuter = curSize;
+        lightSphere.GetComponent<UnityEngine.Rendering.Universal.Light2D>().pointLightOuterRadius = curSize;
     }
 }

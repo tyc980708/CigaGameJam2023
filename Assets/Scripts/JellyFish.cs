9959d94f8da3022bece882e86db9a48d0dba3cd8
Totem <1204025672@qq.com>
2023-07-09 17:56:39 +0800

    音效收尾

--- Assets/Scripts/JellyFish.cs
+++ Assets/Scripts/JellyFish.cs
@@ -385,10 +385,11 @@ public class JellyFish : BaseActor
 
             if (jelly.evoLevel >= evoLevel)
             {
-                AudioManager.PlaySound("Guangdian_Merge_03", lightNum);
-
-                AudioManager.PlaySound("Guangdian_Stinger", lightNum);
-
+                if (isAvatar)
+                {
+                    AudioManager.PlaySound("Guangdian_Merge_03", lightNum);
+                    AudioManager.PlaySound("Guangdian_Stinger", lightNum);
+                }
                 lightNum += 1;
             }
 
